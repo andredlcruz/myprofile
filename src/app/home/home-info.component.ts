256df2c553c6baaceb9d29b7fea241e27448cd13
Dre <andre.dlcruz@gmail.com>
2018-01-09 13:45:07 -0600

    Added info to Home Info compoment

--- src/app/home/home-info.component.ts
+++ src/app/home/home-info.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class HomeInfoComponent implements OnInit {
-  Description = 'Welcome to my web app. It was designed with Angular and Firebase. Looking to enhance my skills in web programing. My main interest is C# and Angular.';
+  Description = 'Welcome to my web app. It was designed with Angular and Firebase';
+  Bio = "Andre De La Cruz graduated from the University of Texas at El Paso in 2013 with a degree in Computer Science. His area of focus includes web development and creating single page apps in Angular and HTML. Other interest includes programming in C# and Java.  A long term goal is to secure a position as a .NET developer. Andre has expressed interest in enhancing his skills in C#, MVC and LINQs. “I would like my career path to include Microsoft technologies and programming with C# and ASP.NET'"
   constructor() {}
 
     ngOnInit() {}
